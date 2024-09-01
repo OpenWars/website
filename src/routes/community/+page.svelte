5ddb3edebd6be7ea5e932d4e57b9d80059e98160
ilaicraftYT <soyilai@proton.me>
2024-09-01 00:00:57 -0600

    feat community: add

---
+++ src/routes/community/+page.svelte
@@ -0,0 +1,16 @@
+<section class="mt-4 mb-4">
+	<h1 class="text-4xl text-center font-black">OpenWars Community</h1>
+	<p class="text-center">Play and chat with other OpenWars' players!</p>
+</section>
+<section class="flex flex-col justify-center items-center">
+	<iframe
+		src="https://discord.com/widget?id=1279345204452917269&theme=dark"
+		title="Discord Widget"
+		width="350"
+		height="500"
+		allowtransparency={true}
+		frameborder="0"
+		sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+	></iframe>
+	<p class="text-center mt-2">More communities coming soon!</p>
+</section>
