8ba54c2ea7fbfb25be2c18b22439c4f2990fbd57
ilaicraftYT <soyilai@proton.me>
2024-09-08 23:55:27 -0600

    refactor resources: add placeholder content

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,2 +1,23 @@
-<p>A</p>
-
+<section class="flex flex-col justify-center items-center h-screen">
+	<div class="border-x border-t border-white px-4 py-2">
+		<h1 class="text-xl font-bold">OpenWars Resource Center</h1>
+	</div>
+	<div class="border border-white p-4 w-1/2">
+		<!--- A main developer was against this idea, but there we go -->
+		<p>Hi! This is the place planned to have all resources for making maps, mods and so.</p>
+		<!--- Total lie: -->
+		<p>But for now, we have planned to implement functionality for maps.</p>
+		<!---
+      At my defense, what I'm supposed to implement? A public list of mods, sprites?
+      Many lawsuits incoming.
+      I also want to avoid authentication and those things, I mean there isn't a single good authentication library for Svelte.
+      There is @authjs but it's too green to be used, i have plans of using a API key instead.
+    -->
+		<p>
+			We could do it right now, but someone <!--- Yes Rivest, it's you --> refuses to make my map format!
+		</p>
+		<br />
+		<p class="text-sm text-zinc-300">(don't open devtools pls)</p>
+		<!--- Written by Ilai at 11:53 P.M. UTC-3, at the point of almost falling sleep on my keyboard. -->
+	</div>
+</section>
